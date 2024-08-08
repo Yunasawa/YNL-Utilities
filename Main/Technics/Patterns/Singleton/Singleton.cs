fa33831f23be048fec15eacda965ef4491222782
Yunasawa <yunasawa100@gmail.com>
2024-08-08 23:40:42 +0700

    Add singleton

---
+++ Main/Technics/Patterns/Singleton/Singleton.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using YNL.Extensions.Methods;
+
+namespace YNL.Patterns.Singletons
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [SerializeField] protected bool _dontDestroyOnLoad = true;
+
+        /// <summary>
+        /// Private static instance.
+        /// </summary>
+        static T _instance;
+
+        /// <summary>
+        /// Public static instance used to refer to Singleton (Eg: SomeClass.Instance)
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                // If no instance is found, find the first GameObject of type T
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    // If no instance exists in the Scene, create a new GameObject and add the Component T 
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject(typeof(T).Name);
+                        _instance = singleton.AddComponent<T>();
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+                // Return the singleton instance
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            GetInstance(true);
+        }
+
+        public void GetInstance(bool dontDestroyOnload)
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                if (dontDestroyOnload)
+                {
+                    var root = transform.root;
+
+                    if (root != transform)
+                    {
+                        if (_dontDestroyOnLoad) DontDestroyOnLoad(root);
+                    }
+                    else
+                    {
+                        if (_dontDestroyOnLoad) DontDestroyOnLoad(this.gameObject);
+                    }
+                }
+            }
+            else
+            {
+                MDebug.Warning($"An instance of <color=#26ffac><b>{gameObject.name}</b></color> has been destroyed.");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
