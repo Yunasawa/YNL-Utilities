ba17e818e225c45f8da4072da7581b77a8088139
Yunasawa <yunasawa100@gmail.com>
2024-08-20 00:04:23 +0700

    1.4.3

---
+++ Setups/YNLUtilitiesSetup.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+
+namespace YNL.Utilities.Setups
+{
+    public class YNLEditorSetup : AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            var inPackages = importedAssets.Any(path => path.StartsWith("Packages/")) ||
+                deletedAssets.Any(path => path.StartsWith("Packages/")) ||
+                movedAssets.Any(path => path.StartsWith("Packages/")) ||
+                movedFromAssetPaths.Any(path => path.StartsWith("Packages/"));
+
+            if (inPackages)
+            {
+                InitializeOnLoad();
+            }
+        }
+
+        public static void InitializeOnLoad()
+        {
+            EditorDefineSymbols.AddSymbols("YNL_UTILITIES");
+        }
+    }
+}
+#endif
